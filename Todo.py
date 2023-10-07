6967266bc44cf7c130a9530064629a3717c2bf8b
wolflergf <wolflergf@gmail.com>
2023-10-07 17:09:40 +0100

    Update

---
+++ Todo.py
@@ -0,0 +1,48 @@
+def get_todos(filepath):
+    with open(filepath, "r") as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+
+def write_todos(filepath, todos_arg):
+    with open(filepath, "w") as file:
+        file.writelines(todos_arg)
+        
+
+
+while True:
+    user_action = input("Type add, show, edit delete or exit: ")
+    user_action = user_action.strip()
+
+    if user_action.startswith("add"):
+        todo = user_action[4:]
+
+        todos = get_todos()
+
+        todos.append(todo + "\n")
+
+        with open("todos.txt", "w") as file:
+            file.writelines(todos)
+        
+    elif user_action.startswith("show"):
+
+        todos = get_todos()
+
+        for index, item in enumerate(todos):
+            item = item.split("\n")
+            row = "{}-{}".format(index + 1, item)
+    elif user_action.startswith("edit"):
+        try:
+            number = int(user_action[5:])
+            print(number)
+
+            number = number - 1
+
+            todos = get_todos()
+
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo + "\n"
+            
+            with open("todos.txt", "w") as file:
+                file.writelines(todos)
---
+++ teste.py
@@ -0,0 +1,5 @@
+def get_person_name(name: str) -> str:
+    return "Hi {}".format(name.title())
+
+
+print(get_person_name("guilherme luciano"))
