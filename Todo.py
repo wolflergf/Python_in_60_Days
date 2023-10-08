9f8aa6c9bd6bc06d336e25d1ddd0ad9c9ea060c1
wolflergf <wolflergf@gmail.com>
2023-10-08 11:19:22 +0100

    update

--- Todo.py
+++ Todo.py
@@ -1,14 +1,12 @@
-def get_todos(filepath):
+def get_todos(filepath="todos.txt"):
     with open(filepath, "r") as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-
 def write_todos(filepath, todos_arg):
     with open(filepath, "w") as file:
         file.writelines(todos_arg)
-        
 
 
 while True:
@@ -22,9 +20,8 @@ while True:
 
         todos.append(todo + "\n")
 
-        with open("todos.txt", "w") as file:
-            file.writelines(todos)
-        
+        write_todos("todos.txt", todos)
+
     elif user_action.startswith("show"):
 
         todos = get_todos()
@@ -43,6 +40,25 @@ while True:
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + "\n"
-            
-            with open("todos.txt", "w") as file:
-                file.writelines(todos)
+
+            write_todos("todos.txt", todos)
+        except ValueError:
+            print("Your command is not valid.")
+            continue
+    elif user_action.startswith("delete"):
+        try:
+            number = int(user_action[7:])
+
+            todos = get_todos()
+            index = number - 1
+            todo_to_remove = todos[index].strip("\n")
+            todos.append(index)
+
+            write_todos("todos.txt", todos)
+
+            messenge = "Todo {} was removed from the list.".format(todo_to_remove)
+            print(messenge)
+
+        except IndexError:
+            print("There is no item with that number")
+            continue
