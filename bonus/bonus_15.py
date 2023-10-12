dac152d219d798c45cbfb5d6a302f4037c4574d6
wolflergf <wolflergf@gmail.com>
2023-10-12 20:06:04 +0100

    finish bonus 15

--- bonus/bonus_15.py
+++ bonus/bonus_15.py
@@ -5,5 +5,23 @@ with open("bonus/files/questions.json", "r") as file:
 
 data = json.loads(content)
 
+
 for quest in data:
     print(quest["quest_text"])
+    for index, alternative in enumerate(quest["alternatives"]):
+        print(index + 1, "-", alternative)
+    user_choise = int(input("Enter your answer: "))
+    quest["user_choise"] = user_choise
+
+score = 0
+for index, quest in enumerate(data):
+    if quest["user_choise"] == quest["correct_answer"]:
+        score += 1
+        result = "Correct Answer"
+    else:
+        result = "Wrong Answer"
+    message = "{} {} - Your answer: {}, Correct answer: {}.".format(index + 1, result, quest["user_choise"], quest["correct_answer"])
+    print(message)
+
+
+print(score, "/", len(data))
