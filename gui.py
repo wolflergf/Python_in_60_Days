d8881f402990ccffb1d4127934a59f2a7cee1e51
wolflergf <wolflergf@gmail.com>
2023-10-15 16:54:24 +0100

    Implement The edit Button

--- gui.py
+++ gui.py
@@ -10,7 +10,8 @@ import PySimpleGUI as sg
 # window.close()
 
 layout = [[sg.Text("Enter a to-do")],
-          [sg.InputText(tooltip="Enter to-do", key="todo"), sg.Button("Add")]]
+          [sg.InputText(tooltip="Enter to-do", key="todo"), sg.Button("Add")],
+          [sg.Listbox(values=functions.get_todos(), key="todos", enable_events=True, size=[45, 10]), sg.Button("Edit")]]
 
 window = sg.Window("My To-Do App", layout, font=("Helvetica", 12))
 
@@ -24,6 +25,17 @@ while True:
             new_todo = values["todo"] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window["todos"].update(values=todos)
+        case "Edit":
+            todo_to_edit = todo = values["todos"][0]
+            new_todo = values["todo"]
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
+        case "todos":
+            window["todo"].update(value=values["todos"][0])
         case sg.WIN_CLOSED:
             break
 
