8216abd97f2b45f5e7e81f38e2cb55947420a0d3
wolflergf <wolflergf@gmail.com>
2023-10-17 19:04:19 +0100

    Create a zip app

--- bonus/bonus_16.py
+++ bonus/bonus_16.py
@@ -1,12 +1,22 @@
 import PySimpleGUI as sg
+from zip_creator import make_archive
 
 
 sg.theme("DarkAmber")
-layout = [[sg.Text("Select files to compress:"), sg.Input(), sg.FilesBrowse("Open")],
-          [sg.Text("Select destination folder:"), sg.Input(), sg.FolderBrowse("Open")],
-          [sg.Button("Compress")]]
+layout = [[sg.Text("Select files to compress:"), sg.Input(), sg.FilesBrowse("Open", key="files")],
+          [sg.Text("Select destination folder:"), sg.Input(), sg.FolderBrowse("Open", key="folder")],
+          [sg.Button("Compress"), sg.Text(key="output")]]
 
 window = sg.Window("My ZIP App", layout)
 
-window.read()
+
+while True:
+    event, values = window.read()
+    print(event, values)
+    filepaths = values["files"].split(";")
+    folder = values["folder"]
+    make_archive(filepaths, folder)
+    window["output"].update(value="Compression Completed!")
+
+
 window.close()
