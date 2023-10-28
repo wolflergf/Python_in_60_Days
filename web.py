02746f57d9eae263883777d9860b2ac1fa6b13e3
wolflergf <wolflergf@gmail.com>
2023-10-29 00:51:06 +0100

    Implement "Add new todo itmes" feature

--- web.py
+++ web.py
@@ -1,7 +1,15 @@
 import streamlit as st
 import functions
 
+
 todos = functions.get_todos()
+
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
@@ -11,6 +19,9 @@ st.write("This app is to increase your productivity.")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label=" ", placeholder="Add a new todo...")
+st.text_input(label=" ", placeholder="Add a new todo...",
+              on_change=add_todo, key="new_todo")
 
 print("Hello")
+
+st.session_state
