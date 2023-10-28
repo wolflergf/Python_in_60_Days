dc56c530b66327917b62024ffeb43070ee3d3d61
wolflergf <wolflergf@gmail.com>
2023-10-28 17:25:29 +0100

    Finish GUI

--- exercise/Convertor.py
+++ exercise/Convertor.py
@@ -2,14 +2,19 @@ import PySimpleGUI as sg
 
 layout = [[sg.Text("Enter feet: "), sg.Input(key="feet")],
           [sg.Text("Enter a inches: "), sg.Input(key="inches")],
-          [sg.Button("Convert"), sg.Text(key="output")]]
+          [sg.Button("Convert"), sg.Button("Exit"), sg.Text(key="output")]]
 
 window = sg.Window("Convertor", layout)
 
 while True:
     event, values = window.read()
+    match event:
+        case "Exit":
+            break
+
     print(event, values)
     meters = (float(values["feet"]) * 0.3048) + (float(values["inches"]) * 0.0254)
     window["output"].update(value="{} meters".format(meters))
+
 
 window.close()
