7472aaed08af0d7f5982127af7fd16e1403c2e0a
wolflergf <wolflergf@gmail.com>
2024-11-29 17:41:10 +0000

    Improving the "Add" Feature.

--- section.09.py
+++ section.09.py
@@ -1,54 +1,52 @@
 while True:
     user_action = input("Type add / show / edit / complete / exit: ").lower().strip()
 
-    match user_action:
-        case "add":
-            todo = input("Enter a todo: ").capitalize().strip() + "\n"
+    if "add" in user_action:
+        todo = user_action[4:].capitalize() + "\n"
 
-            with open("./todos.txt", "r") as file:
-                todos = file.readlines()
+        with open("./todos.txt", "r") as file:
+            todos = file.readlines()
 
-            todos.append(todo)
+        todos.append(todo)
 
-            with open("./todos.txt", "w") as file:
-                file.writelines(todos)
+        with open("./todos.txt", "w") as file:
+            file.writelines(todos)
 
-        case "show":
-            with open("./todos.txt", "r") as file:
-                todos = file.readlines()
+    if "show" in user_action:
+        with open("./todos.txt", "r") as file:
+            todos = file.readlines()
 
-            for index, item in enumerate(todos):
-                row = f"{index + 1}-{item}".strip()
-                print(row)
+        for index, item in enumerate(todos):
+            row = f"{index + 1}-{item}".strip()
+            print(row)
 
-        case "edit":
-            with open("./todos.txt", "r") as file:
-                todos = file.readlines()
+    if "edit" in user_action:
+        with open("./todos.txt", "r") as file:
+            todos = file.readlines()
 
-            for index, item in enumerate(todos):
-                row = f"{index + 1}-{item}".strip()
-                print(row)
-            number = int(input("what number do tou like to replace? ").strip()) - 1
-            todos[number] = input("Enter a new todo: ").capitalize().strip() + "\n"
+        for index, item in enumerate(todos):
+            row = f"{index + 1}-{item}".strip()
+            print(row)
+        number = int(input("what number do tou like to replace? ").strip()) - 1
+        todos[number] = input("Enter a new todo: ").capitalize().strip() + "\n"
 
-            with open("./todos.txt", "w") as file:
-                file.writelines(todos)
+        with open("./todos.txt", "w") as file:
+            file.writelines(todos)
 
-        case "complete":
-            with open("./todos.txt", "r") as file:
-                todos = file.readlines()
+    if "complete" in user_action:
+        with open("./todos.txt", "r") as file:
+            todos = file.readlines()
 
-            for index, item in enumerate(todos):
-                row = f"{index + 1}-{item}".strip()
-                print(row)
-            number = int(input("Number of the todo to complete: ").strip()) - 1
-            todos.pop(number)
+        for index, item in enumerate(todos):
+            row = f"{index + 1}-{item}".strip()
+            print(row)
+        number = int(input("Number of the todo to complete: ").strip()) - 1
+        todos.pop(number)
 
-            with open("./todos.txt", "w") as file:
-                file.writelines(todos)
+        with open("./todos.txt", "w") as file:
+            file.writelines(todos)
 
-        case "exit":
-            break
-        case _:
-            print("Enter a valid command.")
+    if "exit" in user_action:
+        break
+
 print("Bye!")
